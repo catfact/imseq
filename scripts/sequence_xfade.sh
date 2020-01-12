8f53b4e2ec027f98739b6ed91c1f5146ac687c24
ezra buchla <emb@catfact.net>
2020-01-12 00:46:44 -0800

    xfade: lossless temp, force overwrite

--- scripts/sequence_xfade.sh
+++ scripts/sequence_xfade.sh
@@ -8,6 +8,6 @@
 # two-step version
 
 # first, simple upsample: here it is 4 output frames per input stage
-ffmpeg -framerate 0.5 -pattern_type glob -i 'test_frames/*.png' -r 2 -c:v libx264 -crf 25 temp.mp4
+ffmpeg -y -framerate 0.5 -pattern_type glob -i 'test_frames/*.png' -r 2 -c:v libx264 -crf 0 temp.mp4
 # next, apply framerate conversion. interpolation doesn't affect upsamplwed frames, only transitions
-ffmpeg -i temp.mp4 -vf "framerate=fps=24" -codec:v mpeg4 test_xfade.mp4
+ffmpeg -y -i temp.mp4 -vf "framerate=fps=24" -codec:v mpeg4 test_xfade.mp4
